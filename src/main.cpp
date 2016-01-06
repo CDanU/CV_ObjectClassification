b909b8848798746920e3f86452d020d9acb5b563
Daniel Chumak <chumak.daniel.dev@gmail.com>
2016-01-06 16:46:40 +0100

    switch to c++11/opencv

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+// ============================================================================
+// Name        : CV_Ue05_Chumak_Link.cpp
+// Author      : Daniel Chumak, Mario Link
+// Version     :
+// Copyright   : Your copyright notice
+// Description : ...
+// ============================================================================
+
+#include <iostream>
+#include <string.h>
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+// ------------------------------------------------------------------------------
+#define SSTR( x ) dynamic_cast< std::ostringstream& >( ( std::ostringstream() << std::dec << x ) ).str()
+// ------------------------------------------------------------------------------
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    cout << "!!!Hello World!!!" << endl;     // prints !!!Hello World!!!
+    return ( 0);
+}
