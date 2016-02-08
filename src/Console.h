b1f622eceda5a95b4289323295be91a9147f896e
Gilga <gilgatx@gmail.com>
2016-02-08 17:52:36 +0100

    adds feature cmd

--- src/Console.h
+++ src/Console.h
@@ -4,9 +4,12 @@
 
 namespace Ue5
 {
+    std::vector< std::string > GetFeatureNames();
+
     void DoTraining( std::string workpath );
     void DoClassify( std::string imagePath );
     void ShowMatrix( std::string workpath );
+    void ShowFeature( int index, std::string imgpath );
     void Init( std::string appPath );
     void Console();
 }
